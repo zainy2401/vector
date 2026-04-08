ef170984f96312f4fa2c40aeca5e78c392bc7192
zainy2401 <zainuaziz2401@outlook.com>
2026-04-08 23:42:20 +0100

    Major content change
    Major change to content - removed tv oels, decided they are not required for personal reasons. Changed oel to laptop oels, in navigation and on main page

--- www/js/main.js
+++ www/js/main.js
@@ -20,25 +20,6 @@ menuBtn.addEventListener('click', openSidebar);
 closeBtn.addEventListener('click', closeSidebar);
 overlay.addEventListener('click', closeSidebar);
 
-// OEL SWITCHING
-
-function swap(x)
-{
-    const tvContent = document.getElementById("tv");
-    const laptopContent = document.getElementById("laptop");
-    
-    if(x == 1)
-    {
-        tvContent.classList.remove("hidden");
-        laptopContent.classList.add("hidden");
-    }
-    else if(x == 2)
-    {
-        tvContent.classList.add("hidden");
-        laptopContent.classList.remove("hidden");
-    }
-}
-
 // PESONAL SWITCHING
 
 function swap2(x)
